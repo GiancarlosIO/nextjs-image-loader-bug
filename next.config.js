c96401e6547beb3774479f865a756d5a973354c3
Giancarlos Isasi <giancarlos@crehana.com>
2022-02-14 15:55:23 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://my-imgix-cdn.imgix.net'
+  },
+  assetPrefix: 'https://my-custom-cdn.com',
 }
 
 module.exports = nextConfig
